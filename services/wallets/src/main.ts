d1fc8d5f3cb0de4c168f4630a811e71979e44ddf
Cayo Aguiar <cayocan@gmail.com>
2026-05-07 19:32:36 -0300

    wallets: declara process como any em main

--- services/wallets/src/main.ts
+++ services/wallets/src/main.ts
@@ -2,10 +2,12 @@ import "reflect-metadata";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
+declare const process: any;
+
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT;
-  await app.listen(port, "0.0.0.0");
+  const port = process.env.PORT ? parseInt(process.env.PORT as string, 10) : 4000;
+  await app.listen(port, '0.0.0.0');
   console.log(`Wallets service running on port ${port}`);
 }
 
