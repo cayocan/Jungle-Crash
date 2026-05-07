adb32699d5b1e482eff11e9e9fa44dc6373a491f
Cayo Aguiar <cayocan@gmail.com>
2026-05-07 19:32:37 -0300

    wallets: ajusta AppModule

--- services/wallets/src/app.module.ts
+++ services/wallets/src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from "@nestjs/common";
 import { WalletsController } from "./presentation/controllers/wallets.controller";
+import { WalletController } from "./presentation/controllers/wallet.controller";
+import { WalletRepository } from "./repositories/wallet.repository";
 
 @Module({
-  controllers: [WalletsController],
+  controllers: [WalletsController, WalletController],
+  providers: [WalletRepository],
 })
 export class AppModule {}
