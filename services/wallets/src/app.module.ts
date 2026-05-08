56aaf280598018f641d8c88eb5c2db4191e07c9c
Cayo Aguiar <cayocan@gmail.com>
2026-05-07 22:10:50 -0300

    wallets: registra OutboxPublisher e RabbitService no AppModule

--- services/wallets/src/app.module.ts
+++ services/wallets/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from "@nestjs/common";
 import { WalletsController } from "./presentation/controllers/wallets.controller";
 import { WalletController } from "./presentation/controllers/wallet.controller";
 import { WalletRepository } from "./repositories/wallet.repository";
+import { RabbitService } from "./messaging/rabbit.service";
+import { OutboxPublisher } from "./messaging/outbox.publisher";
 
 @Module({
   controllers: [WalletsController, WalletController],
-  providers: [WalletRepository],
+  providers: [WalletRepository, RabbitService, OutboxPublisher],
 })
 export class AppModule {}
