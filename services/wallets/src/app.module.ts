82a4b1b78f223d3a1377386c864d0764a72b453c
Cayo Aguiar <cayocan@gmail.com>
2026-05-07 22:18:10 -0300

    wallets: registra OutboxDlqHandler no AppModule

--- services/wallets/src/app.module.ts
+++ services/wallets/src/app.module.ts
@@ -4,9 +4,10 @@ import { WalletController } from "./presentation/controllers/wallet.controller";
 import { WalletRepository } from "./repositories/wallet.repository";
 import { RabbitService } from "./messaging/rabbit.service";
 import { OutboxPublisher } from "./messaging/outbox.publisher";
+import { OutboxDlqHandler } from "./messaging/outbox.dlq";
 
 @Module({
   controllers: [WalletsController, WalletController],
-  providers: [WalletRepository, RabbitService, OutboxPublisher],
+  providers: [WalletRepository, RabbitService, OutboxPublisher, OutboxDlqHandler],
 })
 export class AppModule {}
