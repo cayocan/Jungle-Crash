627d264aa0892b451814725732013781dda1ab2f
Cayo Aguiar <cayocan@gmail.com>
2026-05-08 01:29:17 -0300

    wallets: registra PrismaClient como provider no AppModule

--- services/wallets/src/app.module.ts
+++ services/wallets/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from "@nestjs/common";
+import { PrismaClient } from '@prisma/client';
 import { WalletsController } from "./presentation/controllers/wallets.controller";
 import { WalletController } from "./presentation/controllers/wallet.controller";
 import { WalletRepository } from "./repositories/wallet.repository";
@@ -9,6 +10,21 @@ import { WalletConsumer } from "./messaging/consumer.service";
 
 @Module({
   controllers: [WalletsController, WalletController],
-  providers: [WalletRepository, RabbitService, OutboxPublisher, OutboxDlqHandler, WalletConsumer],
+  providers: [
+    // PrismaClient provider for Nest DI
+    {
+      provide: PrismaClient,
+      useFactory: () => {
+        const p = new PrismaClient();
+        // do not await connect here to keep startup fast; providers can call $connect if needed
+        return p;
+      },
+    },
+    WalletRepository,
+    RabbitService,
+    OutboxPublisher,
+    OutboxDlqHandler,
+    WalletConsumer,
+  ],
 })
 export class AppModule {}
