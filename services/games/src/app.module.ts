56ed153c1b2da6e552d4addb7e36d7d49a1c044f
Cayo Aguiar <cayocan@gmail.com>
2026-05-08 21:48:04 -0300

    games: registra todos os modulos, providers e inicializacao do servico

--- services/games/src/app.module.ts
+++ services/games/src/app.module.ts
@@ -1,7 +1,28 @@
-import { Module } from "@nestjs/common";
-import { GamesController } from "./presentation/controllers/games.controller";
+import { Module } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { GamesController } from './presentation/controllers/games.controller';
+import { RoundsController } from './presentation/controllers/rounds.controller';
+import { BetController } from './presentation/controllers/bet.controller';
+import { GameGateway } from './presentation/gateways/game.gateway';
+import { RoundRepository } from './repositories/round.repository';
+import { RabbitService } from './messaging/rabbit.service';
+import { OutboxPublisher } from './messaging/outbox.publisher';
+import { WalletEventConsumer } from './messaging/consumer.service';
+import { GameService } from './application/game.service';
 
 @Module({
-  controllers: [GamesController],
+  controllers: [GamesController, RoundsController, BetController],
+  providers: [
+    {
+      provide: PrismaClient,
+      useFactory: () => new PrismaClient(),
+    },
+    RoundRepository,
+    RabbitService,
+    OutboxPublisher,
+    WalletEventConsumer,
+    GameService,
+    GameGateway,
+  ],
 })
 export class AppModule {}
